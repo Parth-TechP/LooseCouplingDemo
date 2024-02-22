686e6cfa46a89af406adf14bd3dc1e2abdccc180
I3-Parth <parth.shah@techprescient.com>
2024-02-23 00:45:26 +0530

    Initial commit

---
+++ src/main/java/com/example/LooseCouplingDemo/LooseCouplingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LooseCouplingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooseCouplingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LooseCouplingDemoApplication.class, args);
+	}
+
+}
